3a861e640c18d7eb5c9f4fdf92d9eda537e2dd79
Evi Vermeêren <evivermeeren@hotmail.com>
2023-10-27 12:20:38 +0200

    feat: import dotenv

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const env = require("dotenv").config();
 
 const mongoose = require("mongoose");
 mongoose.connect(process.env.MONGODB, {
